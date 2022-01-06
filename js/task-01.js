1875e56ca88d73d4e33835e5d1b8da72f61a0622
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-06 07:58:43 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const categItems = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categItems.length}`);
 
-[...categItems].map(item => {
+categItems.forEach(item => {
     
     console.log(
         `Category: ${item.querySelector('h2').textContent}\nElements: ${item.querySelectorAll('li').length}`)
