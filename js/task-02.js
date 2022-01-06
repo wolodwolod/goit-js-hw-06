0f80c836ca24db06f130e50006406131f01c2d65
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-06 08:34:09 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredsEl = document.querySelector('ul#ingredients');
+console.log(ingredsEl);
+
+const makeIngredsEl = options => {
+  return options.map(option => {
+    const liEl = document.createElement('li');
+    
+    liEl.classList.add('item');
+    liEl.textContent = option;
+    
+    return liEl;
+  });
+};
+
+const elements = makeIngredsEl(ingredients);
+ingredsEl.append(...elements);
