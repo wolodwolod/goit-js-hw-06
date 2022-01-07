06aa9c277b4804e4678d21cf35d8c346a8b13fce
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-07 18:55:59 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,29 @@
+const refs = {
+    input: document.querySelector('input#validation-input')
+} 
+
+refs.input.addEventListener('focus', onInputFocus);   
+refs.input.addEventListener('blur', onInputBlur);
+
+function onInputFocus(event) {
+    event.preventDefault();
+    event.currentTarget.classList.remove('valid', 'invalid');
+    // console.log('Инпут получил фокус - событие focus');
+    
+    console.log(event.currentTarget.classList);
+    
+    }
+
+function onInputBlur(event) {
+    event.preventDefault();
+    // console.log('Инпут потерял фокус - событие blur');
+    
+    if (event.currentTarget.value.length !== Number(event.currentTarget.dataset.length)) {
+        event.currentTarget.classList.add('invalid');
+    }
+    else
+    if (event.currentTarget.value.length === Number(event.currentTarget.dataset.length)) {
+        event.currentTarget.classList.add('valid');
+    }
+   console.log(event.currentTarget.classList);
+}
