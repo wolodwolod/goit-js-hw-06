443964985399061f4f909498ff2e03ca7472820a
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-08 13:57:54 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
+
+
+const refs = {
+  body: document.querySelector('body'),
+  button: document.querySelector('button.change-color'),
+  colorLabel: document.querySelector('span.color')
+} 
+
+refs.button.addEventListener('click',onButtonChangeColor);
+
+
+function onButtonChangeColor() {
+
+  refs.body.style.backgroundColor = refs.colorLabel.textContent =  getRandomHexColor();
+  } 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+
